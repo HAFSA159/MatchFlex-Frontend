a7fef3bb21407df4b920289b4e29670e46896a08
HAFSA159 <hafsaelmoatassim99@gmail.com>
2025-02-13 15:28:11 +0100

    navvbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { SmartBandListComponent } from './components/smart-band-list/smart-band-list.component';
 import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/Auth/login/login.component';
+import {RegisterComponent} from './components/Auth/register/register.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'smart-bands', component: SmartBandListComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 
 ];
