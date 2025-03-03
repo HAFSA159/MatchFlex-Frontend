020c5adf04a1d109553ae4d87e14cbb7ce8799e6
HAFSA159 <hafsaelmoatassim99@gmail.com>
2025-03-03 12:21:11 +0000

    table pour manager les utilisateurs

---
+++ src/app/components/features/Admin/user/user.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {SidebarComponent} from "../sidebar/sidebar.component";
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+    imports: [
+        SidebarComponent
+    ],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss'
+})
+export class UserComponent {
+
+}
