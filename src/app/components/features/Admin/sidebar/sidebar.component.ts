5d2039611c3588989bffa8767e6640da1c99728b
HAFSA159 <hafsaelmoatassim99@gmail.com>
2025-03-03 12:23:11 +0000

    fixer le sidebar

--- src/app/components/features/Admin/sidebar/sidebar.component.ts
+++ src/app/components/features/Admin/sidebar/sidebar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  constructor(private router: Router) {}
 
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
 }
