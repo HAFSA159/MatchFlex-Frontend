c62c07657dbf43e560fa2ebc4ca89e2b851592c3
HAFSA159 <hafsaelmoatassim99@gmail.com>
2025-03-03 12:21:48 +0000

    table pour afficher le profil

--- src/app/components/features/Admin/profile/profile.component.ts
+++ src/app/components/features/Admin/profile/profile.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {SidebarComponent} from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    SidebarComponent
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
