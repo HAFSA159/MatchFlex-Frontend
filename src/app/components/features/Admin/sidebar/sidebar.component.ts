e8b57304598f1f379a187cf642b4983373fa6828
HAFSA159 <hafsaelmoatassim99@gmail.com>
2025-03-04 10:05:52 +0000

    add user and display them

--- src/app/components/features/Admin/sidebar/sidebar.component.ts
+++ src/app/components/features/Admin/sidebar/sidebar.component.ts
@@ -1,20 +1,20 @@
-import { Component } from '@angular/core';
-import {Router, RouterLink, RouterLinkActive} from '@angular/router';
+import { Component } from "@angular/core"
+import { Router, RouterLink, RouterLinkActive } from "@angular/router"
+import { CommonModule } from "@angular/common"
 
 @Component({
-  selector: 'app-sidebar',
+  selector: "app-sidebar",
   standalone: true,
-  imports: [
-    RouterLink,
-    RouterLinkActive
-  ],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  providers: [Router],
+  templateUrl: "./sidebar.component.html",
+  styleUrl: "./sidebar.component.scss",
 })
 export class SidebarComponent {
   constructor(private router: Router) {}
 
   isActive(route: string): boolean {
-    return this.router.url === route;
+    return this.router.url === route
   }
 }
+
